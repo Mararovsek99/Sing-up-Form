1c53fbb625e300ad6b3224b268debd9929b1249f
Mararovsek99 <andrej.marovsek99@gmail.com>
2024-05-20 20:15:22 +0200

    added password check in JS and changed background of svg element in the back

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const SubmitBtn = document.querySelector(".btn");
 const password1 = document.querySelector(".pass1").value;
 const password2 = document.querySelector(".pass2").value;
 
-SubmitBtn.addEventListener("click", function(){
+SubmitBtn.addEventListener("click", function(event){
 // after the submit button is pressed, the if function checks the answers,
 //  and with css its change the border of the input
 formControl.forEach(input => {
@@ -14,34 +14,15 @@ formControl.forEach(input => {
         input.classList.add("valid");
     }
     
-    
-})
 
 })
-// when the user get out of password1 field, code checks if other password
-//  is empty and if its not, then checks if the passwords are the same
-// and then calls functions machingPass() or notMachingPass.
 
-document.querySelector('.pass1').addEventListener("blur",function(){
-
-    const password1 = document.querySelector(".pass1").value;
-    const password2 = document.querySelector(".pass2").value;
-
-    if(password2 !== '' & password1 !== ''){
+const password1 = document.querySelector(".pass1").value;
+const password2 = document.querySelector(".pass2").value;
+const PassNotMatchError = document.querySelector("span");
+if(password2 !== '' & password1 !== ''){
         if(password1 !== password2){
-            notMachingPass();
-        }else{
-            machingPass();
-        }
-    }
-})
-document.querySelector('.pass2').addEventListener("blur",function(){
-
-    const password1 = document.querySelector(".pass1").value;
-    const password2 = document.querySelector(".pass2").value;
-
-    if(password2 !== '' & password1 !== ''){
-        if(password1 !== password2){
+            event.preventDefault();
             notMachingPass();
         }else{
             machingPass();
@@ -49,10 +30,27 @@ document.querySelector('.pass2').addEventListener("blur",function(){
     }
 })
 const machingPass = function(){
+    const PassNotMatchError = document.querySelectorAll(".errorMessage");
+
+    document.querySelector('.pass1').classList.remove("invalid");
+    document.querySelector('.pass2').classList.remove("invalid");
     document.querySelector('.pass1').classList.add("valid");
     document.querySelector('.pass2').classList.add("valid");
+
+    PassNotMatchError.forEach(function(errorMassage){
+        errorMassage.style.display = "none" ; 
+    });
+    
 }
 const notMachingPass = function(){
+    const PassNotMatchError = document.querySelectorAll(".errorMessage");
+
+    document.querySelector('.pass1').classList.remove("valid");
+    document.querySelector('.pass2').classList.remove("valid");
     document.querySelector('.pass1').classList.add("invalid");
     document.querySelector('.pass2').classList.add("invalid");
+
+     PassNotMatchError.forEach(function(errorMassage){
+        errorMassage.style.display = "block" ; 
+    });
 }
